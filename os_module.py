8aecf8642560bdbf88ce8166664b8d3b89071c29
Laxmi Gurung <72887242+laxmigurung@users.noreply.github.com>
2024-09-03 18:27:52 -0400

    created os_module.py

---
+++ os_module.py
@@ -0,0 +1,28 @@
+"""
+Create a Python script that counts the number of items (files and directories) in a specified directory(take dir as input). 
+The script should display the total number of items (both files and directories) present in the specified directory. 
+Bonus Modify your script to count the number of files and directories separately. Hint: use the os module"""
+
+# prompt user to enter the directory
+import os
+# get the current working directory
+cwd = os.getcwd() 
+print(f"Your current working directory is {cwd}")
+change_dir = input("Is the file you are looking for is in this directory? (yes/no): ")
+
+while True:
+    if change_dir == "yes":
+        lst = os.listdir(directory)
+        print(f"the total number of files and directories under {cwd} is {len(lst)}.")
+        break
+    elif change_dir == "no":
+        # navigate to root dir
+        os.chdir('../..')
+        os.chdir('cd ubuntu')
+        user_input_file = input("Enter the file/dir path you wish to get the count of: ")
+        os.chdir(user_input_file)
+        break
+
+    else:
+        change_dir = input("Please enter yes or no only!!! Is the file you are looking for is in this directory? (yes/no): ")
+
